c78aa03b73d5cc96f3d9deb911674abb88dbd871
wahabxvii <auwa.mm@gmail.com>
2026-09-18 13:58:14 +0300

    create(ES6_basic): 10-loops.js

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let idx = 0;
+  for (const value of array) {
+    array[idx] = appendString + value;
+    idx += 1;
+  }
+
+  return array;
+}
